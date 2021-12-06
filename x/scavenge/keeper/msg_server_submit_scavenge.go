39668e6fb639db09248d060600b13160e817b6c2
stefan.balea <stefan.balea@axsys.ro>
2021-12-06 18:38:36 +0200

    submit scavenge logic

--- x/scavenge/keeper/msg_server_submit_scavenge.go
+++ x/scavenge/keeper/msg_server_submit_scavenge.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/tendermint/tendermint/crypto"
 
 	"github.com/cosmonaut/bzedgev5/x/scavenge/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,8 +12,36 @@ import (
 func (k msgServer) SubmitScavenge(goCtx context.Context, msg *types.MsgSubmitScavenge) (*types.MsgSubmitScavengeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	var scavenge = types.Scavenge{
+		Index:        msg.SolutionHash,
+		Description:  msg.Description,
+		SolutionHash: msg.SolutionHash,
+		Reward:       msg.Reward,
+		Scavenger:    msg.Creator,
+	}
+
+	_, isFound := k.GetScavenge(ctx, scavenge.SolutionHash)
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Scavenge with the same solution already exists")
+	}
+
+	scavenger, err := sdk.AccAddressFromBech32(scavenge.Scavenger)
+	if err != nil {
+		panic(err)
+	}
+
+	reward, err := sdk.ParseCoinsNormalized(scavenge.Reward)
+	if err != nil {
+		panic(err)
+	}
+
+	moduleAccountAddress := sdk.AccAddress(crypto.AddressHash([]byte(types.ModuleName)))
+	sdkErr := k.bankKeeper.SendCoins(ctx, scavenger, moduleAccountAddress, reward)
+	if sdkErr != nil {
+		return nil, sdkErr
+	}
+
+	k.SetScavenge(ctx, scavenge)
 
 	return &types.MsgSubmitScavengeResponse{}, nil
 }
