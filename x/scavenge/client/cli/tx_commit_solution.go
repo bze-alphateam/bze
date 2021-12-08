778529be8ea6133feac1470f68d3be45735c3bbc
stefan.balea <stefan.balea@axsys.ro>
2021-12-08 13:29:17 +0200

    fix tx commit solution cli

--- x/scavenge/client/cli/tx_commit_solution.go
+++ x/scavenge/client/cli/tx_commit_solution.go
@@ -18,16 +18,17 @@ var _ = strconv.Itoa(0)
 func CmdCommitSolution() *cobra.Command {
 	cmd := &cobra.Command{
 		// pass a solution as the only argument
-		Use:   "commit-solution [solution]",
+		Use:   "commit-solution [scavengeIndex] [solution]",
 		Short: "Broadcast message commit-solution",
 		// set the number of arguments to 1
-		Args: cobra.ExactArgs(1),
+		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
-			solution := args[0]
+			solution := args[1]
+			scavengeIndex := args[0]
 			// find a hash of the solution
 			solutionHash := sha256.Sum256([]byte(solution))
 			// convert the solution hash to string
@@ -35,7 +36,7 @@ func CmdCommitSolution() *cobra.Command {
 			// convert a scavenger address to string
 			var scavenger = clientCtx.GetFromAddress().String()
 			// find the hash of solution and scavenger address
-			var solutionScavengerHash = sha256.Sum256([]byte(solution + scavenger))
+			var solutionScavengerHash = sha256.Sum256([]byte(scavengeIndex + solution + scavenger))
 			// convert the hash to string
 			var solutionScavengerHashString = hex.EncodeToString(solutionScavengerHash[:])
 			// create a new message
