bcc21ecf16317f08103a95e63023824fa343daf8
faneaatiku <faneatiku@yahoo.com>
2022-11-24 00:30:50 +0200

    expose cointrunk queries on rest server

--- x/cointrunk/types/query.pb.gw.go
+++ x/cointrunk/types/query.pb.gw.go
@@ -123,19 +123,26 @@ func local_request_Query_Publisher_0(ctx context.Context, marshaler runtime.Mars
 
 }
 
-var (
-	filter_Query_PublisherByIndex_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_Query_PublisherByIndex_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryPublisherByIndexRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["index"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "index")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_PublisherByIndex_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+
+	protoReq.Index, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "index", err)
 	}
 
 	msg, err := client.PublisherByIndex(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -147,11 +154,22 @@ func local_request_Query_PublisherByIndex_0(ctx context.Context, marshaler runti
 	var protoReq QueryPublisherByIndexRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["index"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "index")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_PublisherByIndex_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+
+	protoReq.Index, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "index", err)
 	}
 
 	msg, err := server.PublisherByIndex(ctx, &protoReq)
@@ -382,13 +400,13 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"bzealphateam", "bze", "cointrunk", "params"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"bze", "cointrunk", "params"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_AcceptedDomain_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"bze-alphateam", "bze", "cointrunk", "accepted_domain"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_AcceptedDomain_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"bze", "cointrunk", "accepted_domain"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_Publisher_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"bze-alphateam", "bze", "cointrunk", "publisher"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_Publisher_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"bze", "cointrunk", "publishers"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_PublisherByIndex_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"bze-alphateam", "bze", "cointrunk", "publisher_by_index"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_PublisherByIndex_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"bze", "cointrunk", "publisher", "index"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
