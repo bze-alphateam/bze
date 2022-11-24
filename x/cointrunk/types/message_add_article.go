d5ddba7d8b680f8215db7e24aadc5f4034360bf0
faneaatiku <faneatiku@yahoo.com>
2022-11-24 16:49:24 +0200

    validate basic on message add article

--- x/cointrunk/types/message_add_article.go
+++ x/cointrunk/types/message_add_article.go
@@ -1,8 +1,11 @@
 package types
 
 import (
+	"errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"net/url"
+	"strings"
 )
 
 const TypeMsgAddArticle = "add_article"
@@ -44,5 +47,37 @@ func (msg *MsgAddArticle) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid publisher address (%s)", err)
 	}
+
+	if len(strings.Trim(msg.Title, " ")) < 10 {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid title: expecting at least 10 characters")
+	}
+
+	err = msg.validateRawURI(msg.Url)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid url provided (%s)", err)
+	}
+
+	//validate picture only if it's provided
+	if msg.Picture == "" {
+		return nil
+	}
+
+	err = msg.validateRawURI(msg.Picture)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid picture url provided (%s)", err)
+	}
+
+	return nil
+}
+
+func (msg *MsgAddArticle) validateRawURI(uri string) error {
+	parsed, err := url.ParseRequestURI(uri)
+	if err != nil {
+		return err
+	}
+	if parsed.Scheme != "https" {
+		return errors.New("invalid url scheme: only https accepted")
+	}
+
 	return nil
 }
