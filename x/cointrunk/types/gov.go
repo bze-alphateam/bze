77284de69e5d91acc6bf31a15efda7190d3d72f6
faneaatiku <faneatiku@yahoo.com>
2023-11-07 19:07:13 +0200

    increased article title limit and fixed domain regex to accept domains longer than 6 letters

--- x/cointrunk/types/gov.go
+++ x/cointrunk/types/gov.go
@@ -43,7 +43,7 @@ func (m *AcceptedDomainProposal) ValidateBasic() error {
 	if err != nil {
 		return err
 	}
-	RegExp := regexp.MustCompile(`^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z
+	RegExp := regexp.MustCompile(`^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,12}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z
  ]{2,3})$`)
 
 	isValidDomain := RegExp.MatchString(m.Domain)
