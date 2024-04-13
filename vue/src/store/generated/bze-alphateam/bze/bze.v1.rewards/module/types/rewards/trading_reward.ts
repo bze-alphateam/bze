c1142e619e603e7dffc6b231687852fa780535f2
faneaatiku <faneatiku@yahoo.com>
2024-04-13 22:20:46 +0200

    updated some dependencies

--- vue/src/store/generated/bze-alphateam/bze/bze.v1.rewards/module/types/rewards/trading_reward.ts
+++ vue/src/store/generated/bze-alphateam/bze/bze.v1.rewards/module/types/rewards/trading_reward.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import { Writer, Reader } from "protobufjs/minimal";
+import * as Long from "long";
+import { util, configure, Writer, Reader } from "protobufjs/minimal";
 
 export const protobufPackage = "bze.v1.rewards";
 
@@ -10,6 +11,34 @@ export interface TradingReward {
   duration: number;
   market_id: string;
   slots: number;
+  expire_at: number;
+}
+
+export interface TradingRewardExpiration {
+  reward_id: string;
+  expire_at: number;
+}
+
+export interface TradingRewardLeaderboard {
+  reward_id: string;
+  list: TradingRewardLeaderboardEntry[];
+}
+
+export interface TradingRewardLeaderboardEntry {
+  amount: string;
+  address: string;
+  created_at: number;
+}
+
+export interface TradingRewardCandidate {
+  reward_id: string;
+  amount: string;
+  address: string;
+}
+
+export interface MarketIdTradingRewardId {
+  reward_id: string;
+  market_id: string;
 }
 
 const baseTradingReward: object = {
@@ -19,6 +48,7 @@ const baseTradingReward: object = {
   duration: 0,
   market_id: "",
   slots: 0,
+  expire_at: 0,
 };
 
 export const TradingReward = {
@@ -40,6 +70,9 @@ export const TradingReward = {
     }
     if (message.slots !== 0) {
       writer.uint32(48).uint32(message.slots);
+    }
+    if (message.expire_at !== 0) {
+      writer.uint32(56).uint32(message.expire_at);
     }
     return writer;
   },
@@ -68,6 +101,9 @@ export const TradingReward = {
           break;
         case 6:
           message.slots = reader.uint32();
+          break;
+        case 7:
+          message.expire_at = reader.uint32();
           break;
         default:
           reader.skipType(tag & 7);
@@ -109,6 +145,11 @@ export const TradingReward = {
     } else {
       message.slots = 0;
     }
+    if (object.expire_at !== undefined && object.expire_at !== null) {
+      message.expire_at = Number(object.expire_at);
+    } else {
+      message.expire_at = 0;
+    }
     return message;
   },
 
@@ -122,6 +163,7 @@ export const TradingReward = {
     message.duration !== undefined && (obj.duration = message.duration);
     message.market_id !== undefined && (obj.market_id = message.market_id);
     message.slots !== undefined && (obj.slots = message.slots);
+    message.expire_at !== undefined && (obj.expire_at = message.expire_at);
     return obj;
   },
 
@@ -157,9 +199,495 @@ export const TradingReward = {
     } else {
       message.slots = 0;
     }
+    if (object.expire_at !== undefined && object.expire_at !== null) {
+      message.expire_at = object.expire_at;
+    } else {
+      message.expire_at = 0;
+    }
     return message;
   },
 };
+
+const baseTradingRewardExpiration: object = { reward_id: "", expire_at: 0 };
+
+export const TradingRewardExpiration = {
+  encode(
+    message: TradingRewardExpiration,
+    writer: Writer = Writer.create()
+  ): Writer {
+    if (message.reward_id !== "") {
+      writer.uint32(10).string(message.reward_id);
+    }
+    if (message.expire_at !== 0) {
+      writer.uint32(16).uint32(message.expire_at);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): TradingRewardExpiration {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = {
+      ...baseTradingRewardExpiration,
+    } as TradingRewardExpiration;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.reward_id = reader.string();
+          break;
+        case 2:
+          message.expire_at = reader.uint32();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): TradingRewardExpiration {
+    const message = {
+      ...baseTradingRewardExpiration,
+    } as TradingRewardExpiration;
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = String(object.reward_id);
+    } else {
+      message.reward_id = "";
+    }
+    if (object.expire_at !== undefined && object.expire_at !== null) {
+      message.expire_at = Number(object.expire_at);
+    } else {
+      message.expire_at = 0;
+    }
+    return message;
+  },
+
+  toJSON(message: TradingRewardExpiration): unknown {
+    const obj: any = {};
+    message.reward_id !== undefined && (obj.reward_id = message.reward_id);
+    message.expire_at !== undefined && (obj.expire_at = message.expire_at);
+    return obj;
+  },
+
+  fromPartial(
+    object: DeepPartial<TradingRewardExpiration>
+  ): TradingRewardExpiration {
+    const message = {
+      ...baseTradingRewardExpiration,
+    } as TradingRewardExpiration;
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = object.reward_id;
+    } else {
+      message.reward_id = "";
+    }
+    if (object.expire_at !== undefined && object.expire_at !== null) {
+      message.expire_at = object.expire_at;
+    } else {
+      message.expire_at = 0;
+    }
+    return message;
+  },
+};
+
+const baseTradingRewardLeaderboard: object = { reward_id: "" };
+
+export const TradingRewardLeaderboard = {
+  encode(
+    message: TradingRewardLeaderboard,
+    writer: Writer = Writer.create()
+  ): Writer {
+    if (message.reward_id !== "") {
+      writer.uint32(10).string(message.reward_id);
+    }
+    for (const v of message.list) {
+      TradingRewardLeaderboardEntry.encode(
+        v!,
+        writer.uint32(18).fork()
+      ).ldelim();
+    }
+    return writer;
+  },
+
+  decode(
+    input: Reader | Uint8Array,
+    length?: number
+  ): TradingRewardLeaderboard {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = {
+      ...baseTradingRewardLeaderboard,
+    } as TradingRewardLeaderboard;
+    message.list = [];
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.reward_id = reader.string();
+          break;
+        case 2:
+          message.list.push(
+            TradingRewardLeaderboardEntry.decode(reader, reader.uint32())
+          );
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): TradingRewardLeaderboard {
+    const message = {
+      ...baseTradingRewardLeaderboard,
+    } as TradingRewardLeaderboard;
+    message.list = [];
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = String(object.reward_id);
+    } else {
+      message.reward_id = "";
+    }
+    if (object.list !== undefined && object.list !== null) {
+      for (const e of object.list) {
+        message.list.push(TradingRewardLeaderboardEntry.fromJSON(e));
+      }
+    }
+    return message;
+  },
+
+  toJSON(message: TradingRewardLeaderboard): unknown {
+    const obj: any = {};
+    message.reward_id !== undefined && (obj.reward_id = message.reward_id);
+    if (message.list) {
+      obj.list = message.list.map((e) =>
+        e ? TradingRewardLeaderboardEntry.toJSON(e) : undefined
+      );
+    } else {
+      obj.list = [];
+    }
+    return obj;
+  },
+
+  fromPartial(
+    object: DeepPartial<TradingRewardLeaderboard>
+  ): TradingRewardLeaderboard {
+    const message = {
+      ...baseTradingRewardLeaderboard,
+    } as TradingRewardLeaderboard;
+    message.list = [];
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = object.reward_id;
+    } else {
+      message.reward_id = "";
+    }
+    if (object.list !== undefined && object.list !== null) {
+      for (const e of object.list) {
+        message.list.push(TradingRewardLeaderboardEntry.fromPartial(e));
+      }
+    }
+    return message;
+  },
+};
+
+const baseTradingRewardLeaderboardEntry: object = {
+  amount: "",
+  address: "",
+  created_at: 0,
+};
+
+export const TradingRewardLeaderboardEntry = {
+  encode(
+    message: TradingRewardLeaderboardEntry,
+    writer: Writer = Writer.create()
+  ): Writer {
+    if (message.amount !== "") {
+      writer.uint32(10).string(message.amount);
+    }
+    if (message.address !== "") {
+      writer.uint32(18).string(message.address);
+    }
+    if (message.created_at !== 0) {
+      writer.uint32(24).int64(message.created_at);
+    }
+    return writer;
+  },
+
+  decode(
+    input: Reader | Uint8Array,
+    length?: number
+  ): TradingRewardLeaderboardEntry {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = {
+      ...baseTradingRewardLeaderboardEntry,
+    } as TradingRewardLeaderboardEntry;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.amount = reader.string();
+          break;
+        case 2:
+          message.address = reader.string();
+          break;
+        case 3:
+          message.created_at = longToNumber(reader.int64() as Long);
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): TradingRewardLeaderboardEntry {
+    const message = {
+      ...baseTradingRewardLeaderboardEntry,
+    } as TradingRewardLeaderboardEntry;
+    if (object.amount !== undefined && object.amount !== null) {
+      message.amount = String(object.amount);
+    } else {
+      message.amount = "";
+    }
+    if (object.address !== undefined && object.address !== null) {
+      message.address = String(object.address);
+    } else {
+      message.address = "";
+    }
+    if (object.created_at !== undefined && object.created_at !== null) {
+      message.created_at = Number(object.created_at);
+    } else {
+      message.created_at = 0;
+    }
+    return message;
+  },
+
+  toJSON(message: TradingRewardLeaderboardEntry): unknown {
+    const obj: any = {};
+    message.amount !== undefined && (obj.amount = message.amount);
+    message.address !== undefined && (obj.address = message.address);
+    message.created_at !== undefined && (obj.created_at = message.created_at);
+    return obj;
+  },
+
+  fromPartial(
+    object: DeepPartial<TradingRewardLeaderboardEntry>
+  ): TradingRewardLeaderboardEntry {
+    const message = {
+      ...baseTradingRewardLeaderboardEntry,
+    } as TradingRewardLeaderboardEntry;
+    if (object.amount !== undefined && object.amount !== null) {
+      message.amount = object.amount;
+    } else {
+      message.amount = "";
+    }
+    if (object.address !== undefined && object.address !== null) {
+      message.address = object.address;
+    } else {
+      message.address = "";
+    }
+    if (object.created_at !== undefined && object.created_at !== null) {
+      message.created_at = object.created_at;
+    } else {
+      message.created_at = 0;
+    }
+    return message;
+  },
+};
+
+const baseTradingRewardCandidate: object = {
+  reward_id: "",
+  amount: "",
+  address: "",
+};
+
+export const TradingRewardCandidate = {
+  encode(
+    message: TradingRewardCandidate,
+    writer: Writer = Writer.create()
+  ): Writer {
+    if (message.reward_id !== "") {
+      writer.uint32(10).string(message.reward_id);
+    }
+    if (message.amount !== "") {
+      writer.uint32(18).string(message.amount);
+    }
+    if (message.address !== "") {
+      writer.uint32(26).string(message.address);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): TradingRewardCandidate {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseTradingRewardCandidate } as TradingRewardCandidate;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.reward_id = reader.string();
+          break;
+        case 2:
+          message.amount = reader.string();
+          break;
+        case 3:
+          message.address = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): TradingRewardCandidate {
+    const message = { ...baseTradingRewardCandidate } as TradingRewardCandidate;
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = String(object.reward_id);
+    } else {
+      message.reward_id = "";
+    }
+    if (object.amount !== undefined && object.amount !== null) {
+      message.amount = String(object.amount);
+    } else {
+      message.amount = "";
+    }
+    if (object.address !== undefined && object.address !== null) {
+      message.address = String(object.address);
+    } else {
+      message.address = "";
+    }
+    return message;
+  },
+
+  toJSON(message: TradingRewardCandidate): unknown {
+    const obj: any = {};
+    message.reward_id !== undefined && (obj.reward_id = message.reward_id);
+    message.amount !== undefined && (obj.amount = message.amount);
+    message.address !== undefined && (obj.address = message.address);
+    return obj;
+  },
+
+  fromPartial(
+    object: DeepPartial<TradingRewardCandidate>
+  ): TradingRewardCandidate {
+    const message = { ...baseTradingRewardCandidate } as TradingRewardCandidate;
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = object.reward_id;
+    } else {
+      message.reward_id = "";
+    }
+    if (object.amount !== undefined && object.amount !== null) {
+      message.amount = object.amount;
+    } else {
+      message.amount = "";
+    }
+    if (object.address !== undefined && object.address !== null) {
+      message.address = object.address;
+    } else {
+      message.address = "";
+    }
+    return message;
+  },
+};
+
+const baseMarketIdTradingRewardId: object = { reward_id: "", market_id: "" };
+
+export const MarketIdTradingRewardId = {
+  encode(
+    message: MarketIdTradingRewardId,
+    writer: Writer = Writer.create()
+  ): Writer {
+    if (message.reward_id !== "") {
+      writer.uint32(10).string(message.reward_id);
+    }
+    if (message.market_id !== "") {
+      writer.uint32(18).string(message.market_id);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): MarketIdTradingRewardId {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = {
+      ...baseMarketIdTradingRewardId,
+    } as MarketIdTradingRewardId;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.reward_id = reader.string();
+          break;
+        case 2:
+          message.market_id = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MarketIdTradingRewardId {
+    const message = {
+      ...baseMarketIdTradingRewardId,
+    } as MarketIdTradingRewardId;
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = String(object.reward_id);
+    } else {
+      message.reward_id = "";
+    }
+    if (object.market_id !== undefined && object.market_id !== null) {
+      message.market_id = String(object.market_id);
+    } else {
+      message.market_id = "";
+    }
+    return message;
+  },
+
+  toJSON(message: MarketIdTradingRewardId): unknown {
+    const obj: any = {};
+    message.reward_id !== undefined && (obj.reward_id = message.reward_id);
+    message.market_id !== undefined && (obj.market_id = message.market_id);
+    return obj;
+  },
+
+  fromPartial(
+    object: DeepPartial<MarketIdTradingRewardId>
+  ): MarketIdTradingRewardId {
+    const message = {
+      ...baseMarketIdTradingRewardId,
+    } as MarketIdTradingRewardId;
+    if (object.reward_id !== undefined && object.reward_id !== null) {
+      message.reward_id = object.reward_id;
+    } else {
+      message.reward_id = "";
+    }
+    if (object.market_id !== undefined && object.market_id !== null) {
+      message.market_id = object.market_id;
+    } else {
+      message.market_id = "";
+    }
+    return message;
+  },
+};
+
+declare var self: any | undefined;
+declare var window: any | undefined;
+var globalThis: any = (() => {
+  if (typeof globalThis !== "undefined") return globalThis;
+  if (typeof self !== "undefined") return self;
+  if (typeof window !== "undefined") return window;
+  if (typeof global !== "undefined") return global;
+  throw "Unable to locate global object";
+})();
 
 type Builtin = Date | Function | Uint8Array | string | number | undefined;
 export type DeepPartial<T> = T extends Builtin
@@ -171,3 +699,15 @@ export type DeepPartial<T> = T extends Builtin
   : T extends {}
   ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
+
+function longToNumber(long: Long): number {
+  if (long.gt(Number.MAX_SAFE_INTEGER)) {
+    throw new globalThis.Error("Value is larger than Number.MAX_SAFE_INTEGER");
+  }
+  return long.toNumber();
+}
+
+if (util.Long !== Long) {
+  util.Long = Long as any;
+  configure();
+}
--- vue/src/store/generated/index.ts
+++ vue/src/store/generated/index.ts
@@ -1,22 +1,58 @@
 // THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
 
-import BzeBurnerV1 from './bze.burner.v1'
-import BzeCointrunkV1 from './bze.cointrunk.v1'
-import BzeEpochsV1 from './bze.epochs.v1'
-import BzeV1Rewards from './bze.v1.rewards'
-import BzeScavenge from './bze.scavenge'
-import BzeTokenfactoryV1 from './bze.tokenfactory.v1'
-import BzeTradebinV1 from './bze.tradebin.v1'
+import BzeAlphateamBzeBzeBurnerV1 from './bze-alphateam/bze/bze.burner.v1'
+import BzeAlphateamBzeBzeCointrunkV1 from './bze-alphateam/bze/bze.cointrunk.v1'
+import BzeAlphateamBzeBzeCointrunk from './bze-alphateam/bze/bze.cointrunk'
+import BzeAlphateamBzeBzeEpochsV1 from './bze-alphateam/bze/bze.epochs.v1'
+import BzeAlphateamBzeBzeScavenge from './bze-alphateam/bze/bze.scavenge'
+import BzeAlphateamBzeBzeTokenfactoryV1 from './bze-alphateam/bze/bze.tokenfactory.v1'
+import BzeAlphateamBzeBzeTradebinV1 from './bze-alphateam/bze/bze.tradebin.v1'
+import BzeAlphateamBzeBzeV1Rewards from './bze-alphateam/bze/bze.v1.rewards'
+import BzeAlphateamBzeBzealphateamBzeBurner from './bze-alphateam/bze/bzealphateam.bze.burner'
+import BzeAlphateamBzeBzealphateamBzeCointrunk from './bze-alphateam/bze/bzealphateam.bze.cointrunk'
+import BzeAlphateamBzeBzealphateamBzeEpochs from './bze-alphateam/bze/bzealphateam.bze.epochs'
+import BzeAlphateamBzeBzealphateamBzeRewards from './bze-alphateam/bze/bzealphateam.bze.rewards'
+import BzeAlphateamBzeBzealphateamBzeTokenfactory from './bze-alphateam/bze/bzealphateam.bze.tokenfactory'
+import BzeAlphateamBzeBzealphateamBzeTradebin from './bze-alphateam/bze/bzealphateam.bze.tradebin'
+import CosmosCosmosSdkCosmosAuthzV1Beta1 from './cosmos/cosmos-sdk/cosmos.authz.v1beta1'
+import CosmosCosmosSdkCosmosBankV1Beta1 from './cosmos/cosmos-sdk/cosmos.bank.v1beta1'
+import CosmosCosmosSdkCosmosCrisisV1Beta1 from './cosmos/cosmos-sdk/cosmos.crisis.v1beta1'
+import CosmosCosmosSdkCosmosDistributionV1Beta1 from './cosmos/cosmos-sdk/cosmos.distribution.v1beta1'
+import CosmosCosmosSdkCosmosEvidenceV1Beta1 from './cosmos/cosmos-sdk/cosmos.evidence.v1beta1'
+import CosmosCosmosSdkCosmosFeegrantV1Beta1 from './cosmos/cosmos-sdk/cosmos.feegrant.v1beta1'
+import CosmosCosmosSdkCosmosGovV1Beta1 from './cosmos/cosmos-sdk/cosmos.gov.v1beta1'
+import CosmosCosmosSdkCosmosSlashingV1Beta1 from './cosmos/cosmos-sdk/cosmos.slashing.v1beta1'
+import CosmosCosmosSdkCosmosStakingV1Beta1 from './cosmos/cosmos-sdk/cosmos.staking.v1beta1'
+import CosmosCosmosSdkCosmosVestingV1Beta1 from './cosmos/cosmos-sdk/cosmos.vesting.v1beta1'
+import CosmosIbcGoIbcApplicationsTransferV1 from './cosmos/ibc-go/ibc.applications.transfer.v1'
 
 
 export default { 
-  BzeBurnerV1: load(BzeBurnerV1, 'bze.burner.v1'),
-  BzeCointrunkV1: load(BzeCointrunkV1, 'bze.cointrunk.v1'),
-  BzeEpochsV1: load(BzeEpochsV1, 'bze.epochs.v1'),
-  BzeV1Rewards: load(BzeV1Rewards, 'bze.v1.rewards'),
-  BzeScavenge: load(BzeScavenge, 'bze.scavenge'),
-  BzeTokenfactoryV1: load(BzeTokenfactoryV1, 'bze.tokenfactory.v1'),
-  BzeTradebinV1: load(BzeTradebinV1, 'bze.tradebin.v1'),
+  BzeAlphateamBzeBzeBurnerV1: load(BzeAlphateamBzeBzeBurnerV1, 'bze.burner.v1'),
+  BzeAlphateamBzeBzeCointrunkV1: load(BzeAlphateamBzeBzeCointrunkV1, 'bze.cointrunk.v1'),
+  BzeAlphateamBzeBzeCointrunk: load(BzeAlphateamBzeBzeCointrunk, 'bze.cointrunk'),
+  BzeAlphateamBzeBzeEpochsV1: load(BzeAlphateamBzeBzeEpochsV1, 'bze.epochs.v1'),
+  BzeAlphateamBzeBzeScavenge: load(BzeAlphateamBzeBzeScavenge, 'bze.scavenge'),
+  BzeAlphateamBzeBzeTokenfactoryV1: load(BzeAlphateamBzeBzeTokenfactoryV1, 'bze.tokenfactory.v1'),
+  BzeAlphateamBzeBzeTradebinV1: load(BzeAlphateamBzeBzeTradebinV1, 'bze.tradebin.v1'),
+  BzeAlphateamBzeBzeV1Rewards: load(BzeAlphateamBzeBzeV1Rewards, 'bze.v1.rewards'),
+  BzeAlphateamBzeBzealphateamBzeBurner: load(BzeAlphateamBzeBzealphateamBzeBurner, 'bzealphateam.bze.burner'),
+  BzeAlphateamBzeBzealphateamBzeCointrunk: load(BzeAlphateamBzeBzealphateamBzeCointrunk, 'bzealphateam.bze.cointrunk'),
+  BzeAlphateamBzeBzealphateamBzeEpochs: load(BzeAlphateamBzeBzealphateamBzeEpochs, 'bzealphateam.bze.epochs'),
+  BzeAlphateamBzeBzealphateamBzeRewards: load(BzeAlphateamBzeBzealphateamBzeRewards, 'bzealphateam.bze.rewards'),
+  BzeAlphateamBzeBzealphateamBzeTokenfactory: load(BzeAlphateamBzeBzealphateamBzeTokenfactory, 'bzealphateam.bze.tokenfactory'),
+  BzeAlphateamBzeBzealphateamBzeTradebin: load(BzeAlphateamBzeBzealphateamBzeTradebin, 'bzealphateam.bze.tradebin'),
+  CosmosCosmosSdkCosmosAuthzV1Beta1: load(CosmosCosmosSdkCosmosAuthzV1Beta1, 'cosmos.authz.v1beta1'),
+  CosmosCosmosSdkCosmosBankV1Beta1: load(CosmosCosmosSdkCosmosBankV1Beta1, 'cosmos.bank.v1beta1'),
+  CosmosCosmosSdkCosmosCrisisV1Beta1: load(CosmosCosmosSdkCosmosCrisisV1Beta1, 'cosmos.crisis.v1beta1'),
+  CosmosCosmosSdkCosmosDistributionV1Beta1: load(CosmosCosmosSdkCosmosDistributionV1Beta1, 'cosmos.distribution.v1beta1'),
+  CosmosCosmosSdkCosmosEvidenceV1Beta1: load(CosmosCosmosSdkCosmosEvidenceV1Beta1, 'cosmos.evidence.v1beta1'),
+  CosmosCosmosSdkCosmosFeegrantV1Beta1: load(CosmosCosmosSdkCosmosFeegrantV1Beta1, 'cosmos.feegrant.v1beta1'),
+  CosmosCosmosSdkCosmosGovV1Beta1: load(CosmosCosmosSdkCosmosGovV1Beta1, 'cosmos.gov.v1beta1'),
+  CosmosCosmosSdkCosmosSlashingV1Beta1: load(CosmosCosmosSdkCosmosSlashingV1Beta1, 'cosmos.slashing.v1beta1'),
+  CosmosCosmosSdkCosmosStakingV1Beta1: load(CosmosCosmosSdkCosmosStakingV1Beta1, 'cosmos.staking.v1beta1'),
+  CosmosCosmosSdkCosmosVestingV1Beta1: load(CosmosCosmosSdkCosmosVestingV1Beta1, 'cosmos.vesting.v1beta1'),
+  CosmosIbcGoIbcApplicationsTransferV1: load(CosmosIbcGoIbcApplicationsTransferV1, 'ibc.applications.transfer.v1'),
   
 }
 
