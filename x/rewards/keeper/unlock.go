cc429a2314040afc4fbeb98e39ab7b795084a6d6
faneaatiku <faneatiku@yahoo.com>
2024-05-30 15:24:03 +0300

    fixed exit from staking reward without locking

--- x/rewards/keeper/unlock.go
+++ x/rewards/keeper/unlock.go
@@ -9,19 +9,8 @@ func (k Keeper) UnlockAllPendingUnlockParticipantsByEpoch(ctx sdk.Context, epoch
 	pending := k.GetAllEpochPendingUnlockParticipant(ctx, epochNumber)
 	for _, p := range pending {
 		logger := k.Logger(ctx).With("pending_unlock", p)
-		partCoins, err := k.getAmountToCapture("", p.Denom, p.Amount, int64(1))
-		if err != nil {
-			logger.Error(err.Error())
-			continue
-		}
 
-		acc, err := sdk.AccAddressFromBech32(p.Address)
-		if err != nil {
-			logger.Error(err.Error())
-			continue
-		}
-
-		err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, acc, partCoins)
+		err := k.performUnlock(ctx, &p)
 		if err != nil {
 			logger.Error(err.Error())
 			continue
@@ -29,4 +18,23 @@ func (k Keeper) UnlockAllPendingUnlockParticipantsByEpoch(ctx sdk.Context, epoch
 
 		k.RemovePendingUnlockParticipant(ctx, p)
 	}
+}
+
+func (k Keeper) performUnlock(ctx sdk.Context, p *types.PendingUnlockParticipant) error {
+	partCoins, err := k.getAmountToCapture("", p.Denom, p.Amount, int64(1))
+	if err != nil {
+		return err
+	}
+
+	acc, err := sdk.AccAddressFromBech32(p.Address)
+	if err != nil {
+		return err
+	}
+
+	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, acc, partCoins)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
