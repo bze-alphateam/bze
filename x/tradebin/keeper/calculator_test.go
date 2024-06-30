fc67ce975b3b48b0ca30f435b32e02f5336d187a
faneaatiku <faneatiku@yahoo.com>
2024-07-01 00:28:56 +0300

    added more test to calculator

--- x/tradebin/keeper/calculator_test.go
+++ x/tradebin/keeper/calculator_test.go
@@ -27,6 +27,12 @@ func (suite *IntegrationTestSuite) TestCalculateMinAmount() {
 
 	minAmount = keeper.CalculateMinAmount("0.0497999")
 	suite.Require().Equal(minAmount.Int64(), int64(42))
+
+	minAmount = keeper.CalculateMinAmount("2.5532")
+	suite.Require().Equal(minAmount.Int64(), int64(2))
+
+	minAmount = keeper.CalculateMinAmount("1.28382713")
+	suite.Require().Equal(minAmount.Int64(), int64(2))
 }
 
 func (suite *IntegrationTestSuite) TestSaveOrder() {
@@ -60,84 +66,152 @@ func (suite *IntegrationTestSuite) TestGetOrderSdkCoin() {
 	suite.Require().Equal(sellCoins.Denom, market.Base)
 }
 
+type GetOrderCoinsWithDustTestCase struct {
+	Price     string
+	BuyDust   float64
+	BuyAmount string
+}
+
 func (suite *IntegrationTestSuite) TestGetOrderCoinsWithDust_UserDoesNotReceive() {
-	price := "0.91"
-	minAmount := keeper.CalculateMinAmount(price)
-	coinReq := types.OrderCoinsArguments{
-		OrderType:    types.OrderTypeBuy,
-		OrderPrice:   price,
-		OrderAmount:  minAmount,
-		Market:       &market,
-		UserAddress:  "addr1",
-		UserReceives: false,
-	}
-	buyCoins, err := suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
-	suite.Require().Nil(err)
-	suite.Require().Equal(buyCoins.Coin.Amount.Int64(), int64(4)) //result of amount * price
-	suite.Require().Equal(buyCoins.Coin.Denom, market.Quote)
-	suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), 0.36)
-	dustFromStorage, err := sdk.NewDecFromStr(buyCoins.UserDust.Amount)
-	suite.Require().Nil(err)
-	suite.Require().EqualValues(buyCoins.Dust, dustFromStorage)
-	suite.Require().EqualValues(buyCoins.UserDust.Owner, "addr1")
-	suite.Require().EqualValues(buyCoins.UserDust.Denom, market.Quote)
-
-	coinReq = types.OrderCoinsArguments{
-		OrderType:    types.OrderTypeSell,
-		OrderPrice:   price,
-		OrderAmount:  minAmount,
-		Market:       &market,
-		UserAddress:  "addr2",
-		UserReceives: false,
+	tc := []GetOrderCoinsWithDustTestCase{
+		{
+			Price:     "0.91",
+			BuyDust:   0.36,
+			BuyAmount: "4",
+		},
+		{
+			Price:     "0.00004",
+			BuyDust:   0,
+			BuyAmount: "2",
+		},
+		{
+			Price:     "0.06617",
+			BuyDust:   0.88256,
+			BuyAmount: "3",
+		},
+		{
+			Price:     "0.13891",
+			BuyDust:   0.77744,
+			BuyAmount: "3",
+		},
+		{
+			Price:     "2.118322",
+			BuyDust:   0.763356,
+			BuyAmount: "5",
+		},
 	}
 
-	buyCoins, err = suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
-	suite.Require().Nil(err)
-	suite.Require().Equal(buyCoins.Coin.Amount.Int64(), int64(4)) //result of amount * price
-	suite.Require().Equal(buyCoins.Coin.Denom, market.Base)
-	suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), 0)
-	suite.Require().Nil(err)
-	suite.Require().Nil(buyCoins.UserDust)
-	suite.Require().EqualValues(buyCoins.Dust, sdk.ZeroDec())
+	for _, tc := range tc {
+		minAmount := keeper.CalculateMinAmount(tc.Price)
+		coinReq := types.OrderCoinsArguments{
+			OrderType:    types.OrderTypeBuy,
+			OrderPrice:   tc.Price,
+			OrderAmount:  minAmount,
+			Market:       &market,
+			UserAddress:  "addr1",
+			UserReceives: false,
+		}
+		buyCoins, err := suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
+		suite.Require().Nil(err)
+		suite.Require().Equal(buyCoins.Coin.Amount.String(), tc.BuyAmount) //result of amount * price
+		suite.Require().Equal(buyCoins.Coin.Denom, market.Quote)
+		suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), tc.BuyDust)
+		if tc.BuyDust > 0 {
+			dustFromStorage, err := sdk.NewDecFromStr(buyCoins.UserDust.Amount)
+			suite.Require().Nil(err)
+			suite.Require().EqualValues(buyCoins.Dust, dustFromStorage)
+			suite.Require().EqualValues(buyCoins.UserDust.Owner, "addr1")
+			suite.Require().EqualValues(buyCoins.UserDust.Denom, market.Quote)
+		}
+
+		coinReq = types.OrderCoinsArguments{
+			OrderType:    types.OrderTypeSell,
+			OrderPrice:   tc.Price,
+			OrderAmount:  minAmount,
+			Market:       &market,
+			UserAddress:  "addr2",
+			UserReceives: false,
+		}
+
+		buyCoins, err = suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
+		suite.Require().Nil(err)
+		suite.Require().Equal(buyCoins.Coin.Amount, minAmount)
+		suite.Require().Equal(buyCoins.Coin.Denom, market.Base)
+		suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), 0)
+		suite.Require().Nil(err)
+		suite.Require().Nil(buyCoins.UserDust)
+		suite.Require().EqualValues(buyCoins.Dust, sdk.ZeroDec())
+	}
 }
 
 func (suite *IntegrationTestSuite) TestGetOrderCoinsWithDust_UserReceive() {
-	price := "0.91"
-	minAmount := keeper.CalculateMinAmount(price)
-	coinReq := types.OrderCoinsArguments{
-		OrderType:    types.OrderTypeBuy,
-		OrderPrice:   price,
-		OrderAmount:  minAmount,
-		Market:       &market,
-		UserAddress:  "addr1",
-		UserReceives: true,
-	}
-	buyCoins, err := suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
-	suite.Require().Nil(err)
-	suite.Require().Equal(buyCoins.Coin.Amount.Int64(), int64(3)) //result of amount * price
-	suite.Require().Equal(buyCoins.Coin.Denom, market.Quote)
-	suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), 0.64)
-	dustFromStorage, err := sdk.NewDecFromStr(buyCoins.UserDust.Amount)
-	suite.Require().Nil(err)
-	suite.Require().EqualValues(buyCoins.Dust, dustFromStorage)
-	suite.Require().EqualValues(buyCoins.UserDust.Owner, "addr1")
-	suite.Require().EqualValues(buyCoins.UserDust.Denom, market.Quote)
-
-	coinReq = types.OrderCoinsArguments{
-		OrderType:    types.OrderTypeSell,
-		OrderPrice:   price,
-		OrderAmount:  minAmount,
-		Market:       &market,
-		UserAddress:  "addr2",
-		UserReceives: false,
+	tc := []GetOrderCoinsWithDustTestCase{
+		{
+			Price:     "0.91",
+			BuyDust:   0.64,
+			BuyAmount: "3",
+		},
+		{
+			Price:     "0.00004",
+			BuyDust:   0,
+			BuyAmount: "2",
+		},
+		{
+			Price:     "0.06617",
+			BuyDust:   0.11744,
+			BuyAmount: "2",
+		},
+		{
+			Price:     "0.13891",
+			BuyDust:   0.22256,
+			BuyAmount: "2",
+		},
+		{
+			Price:     "2.118322",
+			BuyDust:   0.236644,
+			BuyAmount: "4",
+		},
 	}
 
-	buyCoins, err = suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
-	suite.Require().Nil(err)
-	suite.Require().Equal(buyCoins.Coin.Amount.Int64(), int64(4)) //result of amount * price
-	suite.Require().Equal(buyCoins.Coin.Denom, market.Base)
-	suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), 0)
-	suite.Require().Nil(err)
-	suite.Require().Nil(buyCoins.UserDust)
-	suite.Require().EqualValues(buyCoins.Dust, sdk.ZeroDec())
+	for _, tc := range tc {
+		minAmount := keeper.CalculateMinAmount(tc.Price)
+		coinReq := types.OrderCoinsArguments{
+			OrderType:    types.OrderTypeBuy,
+			OrderPrice:   tc.Price,
+			OrderAmount:  minAmount,
+			Market:       &market,
+			UserAddress:  "addr1",
+			UserReceives: true,
+		}
+		buyCoins, err := suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
+		suite.Require().Nil(err)
+		suite.Require().Equal(buyCoins.Coin.Amount.String(), tc.BuyAmount)
+		suite.Require().Equal(buyCoins.Coin.Denom, market.Quote)
+		suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), tc.BuyDust)
+		if tc.BuyDust > 0 {
+			dustFromStorage, err := sdk.NewDecFromStr(buyCoins.UserDust.Amount)
+			suite.Require().Nil(err)
+			suite.Require().EqualValues(buyCoins.Dust, dustFromStorage)
+			suite.Require().EqualValues(buyCoins.UserDust.Owner, "addr1")
+			suite.Require().EqualValues(buyCoins.UserDust.Denom, market.Quote)
+		}
+
+		coinReq = types.OrderCoinsArguments{
+			OrderType:    types.OrderTypeSell,
+			OrderPrice:   tc.Price,
+			OrderAmount:  minAmount,
+			Market:       &market,
+			UserAddress:  "addr2",
+			UserReceives: false,
+		}
+
+		buyCoins, err = suite.k.GetOrderCoinsWithDust(suite.ctx, coinReq)
+		suite.Require().Nil(err)
+		suite.Require().Equal(buyCoins.Coin.Amount.String(), minAmount.String())
+		suite.Require().Equal(buyCoins.Coin.Denom, market.Base)
+		suite.Require().EqualValues(buyCoins.Dust.MustFloat64(), 0.0)
+		suite.Require().Nil(err)
+		suite.Require().Nil(buyCoins.UserDust)
+		suite.Require().EqualValues(buyCoins.Dust, sdk.ZeroDec())
+	}
 }
