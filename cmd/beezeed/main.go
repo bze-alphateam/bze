ba946f6eed913781bdbc8ebbbd2f11ce0dda1f70
Developer Experience team at Ignite <hello@ignite.com>
2025-01-29 16:50:58 +0200

    Initialized with Ignite CLI

---
+++ cmd/beezeed/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	clienthelpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"beezee/app"
+	"beezee/cmd/beezeed/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clienthelpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
