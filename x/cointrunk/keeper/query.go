43a3b42485bac80e2dd288bdb5bf685745c37dc2
stefan.balea <stefan.balea@axsys.ro>
2025-04-26 02:08:04 +0300

    added cointrunk quries handlers

--- x/cointrunk/keeper/query.go
+++ x/cointrunk/keeper/query.go
@@ -2,8 +2,10 @@ package keeper
 
 import (
 	"context"
+	"encoding/binary"
 	"github.com/bze-alphateam/bze/x/cointrunk/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -16,11 +18,23 @@ func (k Keeper) AcceptedDomain(goCtx context.Context, req *types.QueryAcceptedDo
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	var acceptedDomains []types.AcceptedDomain
+	adStore := k.getPrefixedStore(ctx, types.KeyPrefix(types.AcceptedDomainKeyPrefix))
 
-	// TODO: Process the query
-	_ = ctx
+	pageRes, err := query.Paginate(adStore, req.Pagination, func(key []byte, value []byte) error {
+		var acceptedDomain types.AcceptedDomain
+		if err := k.cdc.Unmarshal(value, &acceptedDomain); err != nil {
+			return err
+		}
+		acceptedDomains = append(acceptedDomains, acceptedDomain)
+		return nil
+	})
 
-	return &types.QueryAcceptedDomainResponse{}, nil
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAcceptedDomainResponse{AcceptedDomain: acceptedDomains, Pagination: pageRes}, nil
 }
 
 func (k Keeper) AllAnonArticlesCounters(goCtx context.Context, req *types.QueryAllAnonArticlesCountersRequest) (*types.QueryAllAnonArticlesCountersResponse, error) {
@@ -29,11 +43,23 @@ func (k Keeper) AllAnonArticlesCounters(goCtx context.Context, req *types.QueryA
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	var counters []types.AnonArticlesCounter
+	countersStore := k.getPrefixedStore(ctx, types.KeyPrefix(types.AnonArticlesCounterKeyPrefix))
+	pageRes, err := query.Paginate(countersStore, req.Pagination, func(key []byte, value []byte) error {
+		var counter = types.AnonArticlesCounter{
+			Key:     string(key[:]),
+			Counter: binary.BigEndian.Uint64(value),
+		}
 
-	// TODO: Process the query
-	_ = ctx
+		counters = append(counters, counter)
+		return nil
+	})
 
-	return &types.QueryAllAnonArticlesCountersResponse{}, nil
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllAnonArticlesCountersResponse{AnonArticlesCounters: counters, Pagination: pageRes}, nil
 }
 
 func (k Keeper) AllArticles(goCtx context.Context, req *types.QueryAllArticlesRequest) (*types.QueryAllArticlesResponse, error) {
@@ -42,11 +68,22 @@ func (k Keeper) AllArticles(goCtx context.Context, req *types.QueryAllArticlesRe
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	var articles []types.Article
+	articlesStore := k.getPrefixedStore(ctx, types.KeyPrefix(types.ArticleKeyPrefix))
+	pageRes, err := query.Paginate(articlesStore, req.Pagination, func(key []byte, value []byte) error {
+		var article types.Article
+		if err := k.cdc.Unmarshal(value, &article); err != nil {
+			return err
+		}
+		articles = append(articles, article)
+		return nil
+	})
 
-	// TODO: Process the query
-	_ = ctx
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
 
-	return &types.QueryAllArticlesResponse{}, nil
+	return &types.QueryAllArticlesResponse{Article: articles, Pagination: pageRes}, nil
 }
 
 func (k Keeper) Publisher(goCtx context.Context, req *types.QueryPublisherRequest) (*types.QueryPublisherResponse, error) {
@@ -56,10 +93,12 @@ func (k Keeper) Publisher(goCtx context.Context, req *types.QueryPublisherReques
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	publisher, found := k.GetPublisher(ctx, req.Address)
+	if !found {
+		return nil, status.Error(codes.InvalidArgument, "not found")
+	}
 
-	return &types.QueryPublisherResponse{}, nil
+	return &types.QueryPublisherResponse{Publisher: &publisher}, nil
 }
 
 func (k Keeper) Publishers(goCtx context.Context, req *types.QueryPublishersRequest) (*types.QueryPublishersResponse, error) {
@@ -68,9 +107,20 @@ func (k Keeper) Publishers(goCtx context.Context, req *types.QueryPublishersRequ
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	var publishers []types.Publisher
+	publisherStore := k.getPrefixedStore(ctx, types.KeyPrefix(types.PublisherKeyPrefix))
+	pageRes, err := query.Paginate(publisherStore, req.Pagination, func(key []byte, value []byte) error {
+		var publisher types.Publisher
+		if err := k.cdc.Unmarshal(value, &publisher); err != nil {
+			return err
+		}
+		publishers = append(publishers, publisher)
+		return nil
+	})
 
-	// TODO: Process the query
-	_ = ctx
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
 
-	return &types.QueryPublishersResponse{}, nil
+	return &types.QueryPublishersResponse{Publisher: publishers, Pagination: pageRes}, nil
 }
