30516049095cd41af19871fe6e7c1d2fd3d004b6
stefan.balea <stefan.balea@axsys.ro>
2025-04-25 18:28:24 +0300

    added burner msgs and queries

--- x/burner/keeper/msg_server.go
+++ x/burner/keeper/msg_server.go
@@ -2,17 +2,13 @@ package keeper
 
 import (
 	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/bze-alphateam/bze/x/burner/types"
 )
 
 type msgServer struct {
 	Keeper
-}
-
-func (k msgServer) FundBurner(ctx context.Context, burner *types.MsgFundBurner) (*types.MsgFundBurnerResponse, error) {
-	//TODO implement me
-	panic("implement me")
 }
 
 // NewMsgServerImpl returns an implementation of the MsgServer interface
@@ -22,3 +18,28 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 }
 
 var _ types.MsgServer = msgServer{}
+
+func (k msgServer) FundBurner(goCtx context.Context, msg *types.MsgFundBurner) (*types.MsgFundBurnerResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	amount, err := sdk.ParseCoinsNormalized(msg.Amount)
+	if err != nil {
+		return nil, err
+	}
+
+	creatorAccount, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+
+	err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, creatorAccount, types.ModuleName, amount)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ctx.EventManager().EmitTypedEvent(&types.FundBurnerEvent{From: msg.Creator, Amount: amount.String()})
+	if err != nil {
+		ctx.Logger().Error("failed to emit fund burner event", "error", err)
+	}
+
+	return &types.MsgFundBurnerResponse{}, nil
+}
--- x/burner/keeper/query.go
+++ x/burner/keeper/query.go
@@ -2,23 +2,93 @@ package keeper
 
 import (
 	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/bze-alphateam/bze/x/burner/types"
 )
 
 var _ types.QueryServer = Keeper{}
 
-func (k Keeper) Raffles(ctx context.Context, request *types.QueryRafflesRequest) (*types.QueryRafflesResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (k Keeper) Raffles(goCtx context.Context, request *types.QueryRafflesRequest) (*types.QueryRafflesResponse, error) {
+	if request == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	var items []types.Raffle
+	raffleStore := k.getPrefixedStore(ctx, types.KeyPrefix(types.RaffleKeyPrefix))
+	pageRes, err := query.Paginate(raffleStore, request.Pagination, func(key []byte, value []byte) error {
+		var entry types.Raffle
+		if err := k.cdc.Unmarshal(value, &entry); err != nil {
+
+			return err
+		}
+		items = append(items, entry)
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryRafflesResponse{List: items, Pagination: pageRes}, nil
 }
 
-func (k Keeper) RaffleWinners(ctx context.Context, request *types.QueryRaffleWinnersRequest) (*types.QueryRaffleWinnersResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (k Keeper) RaffleWinners(goCtx context.Context, request *types.QueryRaffleWinnersRequest) (*types.QueryRaffleWinnersResponse, error) {
+	if request == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if request.Denom == "" {
+		return nil, status.Error(codes.InvalidArgument, "denom required")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	var items []types.RaffleWinner
+	raffleStore := k.getPrefixedStore(ctx, types.KeyPrefix(string(types.GetRaffleWinnerKeyPrefix(request.Denom))))
+	pageRes, err := query.Paginate(raffleStore, request.Pagination, func(key []byte, value []byte) error {
+		var entry types.RaffleWinner
+		if err := k.cdc.Unmarshal(value, &entry); err != nil {
+
+			return err
+		}
+		items = append(items, entry)
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryRaffleWinnersResponse{List: items, Pagination: pageRes}, nil
 }
 
-func (k Keeper) AllBurnedCoins(ctx context.Context, request *types.QueryAllBurnedCoinsRequest) (*types.QueryAllBurnedCoinsResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (k Keeper) AllBurnedCoins(goCtx context.Context, request *types.QueryAllBurnedCoinsRequest) (*types.QueryAllBurnedCoinsResponse, error) {
+	if request == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	var burnedCoins []types.BurnedCoins
+	store := k.getPrefixedStore(ctx, types.KeyPrefix(types.BurnedCoinsKeyPrefix))
+	pageRes, err := query.Paginate(store, request.Pagination, func(key []byte, value []byte) error {
+		var entry types.BurnedCoins
+		if err := k.cdc.Unmarshal(value, &entry); err != nil {
+			return err
+		}
+		burnedCoins = append(burnedCoins, entry)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllBurnedCoinsResponse{BurnedCoins: burnedCoins, Pagination: pageRes}, nil
 }
