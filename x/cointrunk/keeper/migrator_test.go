8f77bcaed4fa13b7a154ccacfa6312e08726a36c
stefan.balea <stefan.balea@axsys.ro>
2025-05-30 01:44:19 +0300

    added migration tests on cointrunk

---
+++ x/cointrunk/keeper/migrator_test.go
@@ -0,0 +1,57 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"testing"
+
+	keeper2 "github.com/bze-alphateam/bze/testutil/keeper"
+	"github.com/bze-alphateam/bze/x/cointrunk/exported"
+	"github.com/bze-alphateam/bze/x/cointrunk/keeper"
+	"github.com/bze-alphateam/bze/x/cointrunk/testutil"
+	"github.com/bze-alphateam/bze/x/cointrunk/types"
+)
+
+// mockSubspace implements the exported.Subspace interface for testing
+type mockSubspace struct {
+	ps types.Params
+}
+
+func newMockSubspace(ps types.Params) mockSubspace {
+	return mockSubspace{ps: ps}
+}
+
+func (ms mockSubspace) GetParamSet(_ sdk.Context, ps exported.ParamSet) {
+	*ps.(*types.Params) = ms.ps
+}
+
+// TestMigrator_Migrate1to2 tests the successful migration from version 1 to 2
+func TestMigrator_Migrate1to2(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockBank := testutil.NewMockBankKeeper(mockCtrl)
+	require.NotNil(t, mockBank)
+	mockDistr := testutil.NewMockDistrKeeper(mockCtrl)
+	require.NotNil(t, mockDistr)
+
+	k, ctx := keeper2.CointrunkKeeper(t, mockBank, mockDistr)
+
+	// Create mock subspace with default params
+	legacySubspace := newMockSubspace(types.DefaultParams())
+
+	// Create migrator
+	migrator := keeper.NewMigrator(k, legacySubspace)
+
+	// Run migration (testing with empty publisher store since we can't inject v1 data)
+	require.NoError(t, migrator.Migrate1to2(ctx))
+
+	// Verify params were migrated
+	params := k.GetParams(ctx)
+	require.Equal(t, legacySubspace.ps, params)
+
+	// Verify no publishers exist (since we started with empty store)
+	allPublishers := k.GetAllPublisher(ctx)
+	require.Empty(t, allPublishers)
+}
--- x/cointrunk/migrations/v2/migrator_test.go
+++ x/cointrunk/migrations/v2/migrator_test.go
@@ -3,6 +3,7 @@ package v2_test
 import (
 	ct "github.com/bze-alphateam/bze/x/cointrunk/module"
 	"testing"
+	"time"
 
 	"cosmossdk.io/store/prefix"
 	storetypes "cosmossdk.io/store/types"
@@ -14,6 +15,7 @@ import (
 	"github.com/bze-alphateam/bze/x/cointrunk/exported"
 	v2 "github.com/bze-alphateam/bze/x/cointrunk/migrations/v2"
 	"github.com/bze-alphateam/bze/x/cointrunk/types"
+	"github.com/bze-alphateam/bze/x/cointrunk/v1types"
 )
 
 // mockSubspace implements the exported.Subspace interface for testing
@@ -29,8 +31,8 @@ func (ms mockSubspace) GetParamSet(_ sdk.Context, ps exported.ParamSet) {
 	*ps.(*types.Params) = ms.ps
 }
 
-// TestMigrate tests the successful migration of params from legacy subspace to module store
-func TestMigrate(t *testing.T) {
+// TestMigrateParams tests the successful migration of params from legacy subspace to module store
+func TestMigrateParams(t *testing.T) {
 	encCfg := moduletestutil.MakeTestEncodingConfig(ct.AppModuleBasic{})
 	cdc := encCfg.Codec
 
@@ -44,7 +46,7 @@ func TestMigrate(t *testing.T) {
 	legacySubspace := newMockSubspace(types.DefaultParams())
 
 	// Run migration
-	require.NoError(t, v2.Migrate(ctx, store, legacySubspace, cdc))
+	require.NoError(t, v2.MigrateParams(ctx, store, legacySubspace, cdc))
 
 	// Verify params were stored correctly
 	var res types.Params
@@ -52,4 +54,79 @@ func TestMigrate(t *testing.T) {
 	require.NotNil(t, bz, "params should be stored in the new location")
 	require.NoError(t, cdc.Unmarshal(bz, &res))
 	require.Equal(t, legacySubspace.ps, res)
+}
+
+// TestMigratePublishers tests the migration of publishers from v1 to v2 format
+func TestMigratePublishers(t *testing.T) {
+	encCfg := moduletestutil.MakeTestEncodingConfig(ct.AppModuleBasic{})
+	cdc := encCfg.Codec
+
+	storeKey := storetypes.NewKVStoreKey(types.ModuleName)
+	tKey := storetypes.NewTransientStoreKey("transient_test")
+	ctx := testutil.DefaultContext(storeKey, tKey)
+
+	store := prefix.NewStore(ctx.KVStore(storeKey), []byte{})
+
+	// Create v1 publishers with int64 respect field
+	v1Publishers := []v1types.Publisher{
+		{
+			Name:          "Publisher One",
+			Address:       "address1",
+			Active:        true,
+			ArticlesCount: 5,
+			CreatedAt:     time.Now().Unix(),
+			Respect:       1000,
+		},
+		{
+			Name:          "Publisher Two",
+			Address:       "address2",
+			Active:        false,
+			ArticlesCount: 10,
+			CreatedAt:     time.Now().Unix(),
+			Respect:       -500,
+		},
+		{
+			Name:          "Publisher Three",
+			Address:       "address3",
+			Active:        true,
+			ArticlesCount: 0,
+			CreatedAt:     time.Now().Unix(),
+			Respect:       0,
+		},
+	}
+
+	// Store v1 publishers in the store
+	for _, pub := range v1Publishers {
+		bz := cdc.MustMarshal(&pub)
+		store.Set(types.PublisherKey(pub.Address), bz)
+	}
+
+	// Run migration
+	require.NoError(t, v2.MigratePublishers(store, cdc))
+
+	// Verify publishers were migrated correctly
+	for _, v1Pub := range v1Publishers {
+		// Get migrated publisher
+		bz := store.Get(types.PublisherKey(v1Pub.Address))
+		require.NotNil(t, bz, "publisher should exist after migration")
+
+		var v2Pub types.Publisher
+		require.NoError(t, cdc.Unmarshal(bz, &v2Pub))
+
+		// Verify all fields were migrated correctly
+		require.Equal(t, v1Pub.Name, v2Pub.Name)
+		require.Equal(t, v1Pub.Address, v2Pub.Address)
+		require.Equal(t, v1Pub.Active, v2Pub.Active)
+		require.Equal(t, v1Pub.ArticlesCount, v2Pub.ArticlesCount)
+		require.Equal(t, v1Pub.CreatedAt, v2Pub.CreatedAt)
+
+		// Verify respect was converted from int64 to string
+		expectedRespect := "1000"
+		if v1Pub.Address == "address2" {
+			expectedRespect = "-500"
+		} else if v1Pub.Address == "address3" {
+			expectedRespect = "0"
+		}
+		require.Equal(t, expectedRespect, v2Pub.Respect)
+	}
 }
