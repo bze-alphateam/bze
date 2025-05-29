49805ffd482ab1d3982b54141249e370e49ee2d6
stefan.balea <stefan.balea@axsys.ro>
2025-05-29 14:10:29 +0300

    more tradebin tests

--- x/tradebin/keeper/msg_update_params_test.go
+++ x/tradebin/keeper/msg_update_params_test.go
@@ -1,1 +1,270 @@
 package keeper_test
+
+import (
+	"github.com/bze-alphateam/bze/x/tradebin/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+)
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_ValidAuthority() {
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	params := types.Params{
+		CreateMarketFee:     "1000",
+		MarketMakerFee:      "0.001",
+		MarketTakerFee:      "0.002",
+		MakerFeeDestination: "community_pool",
+		TakerFeeDestination: "burn",
+	}
+
+	msg := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    params,
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+
+	// Verify params were updated
+	retrievedParams := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal(params.CreateMarketFee, retrievedParams.CreateMarketFee)
+	suite.Require().Equal(params.MarketMakerFee, retrievedParams.MarketMakerFee)
+	suite.Require().Equal(params.MarketTakerFee, retrievedParams.MarketTakerFee)
+	suite.Require().Equal(params.MakerFeeDestination, retrievedParams.MakerFeeDestination)
+	suite.Require().Equal(params.TakerFeeDestination, retrievedParams.TakerFeeDestination)
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_InvalidAuthority() {
+	invalidAuthority := "bze1invalidauthority"
+	params := types.Params{
+		CreateMarketFee:     "1000",
+		MarketMakerFee:      "0.001",
+		MarketTakerFee:      "0.002",
+		MakerFeeDestination: "community_pool",
+		TakerFeeDestination: "burn",
+	}
+
+	msg := &types.MsgUpdateParams{
+		Authority: invalidAuthority,
+		Params:    params,
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().Error(err)
+	suite.Require().Nil(response)
+	suite.Require().Contains(err.Error(), "invalid authority")
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_EmptyAuthority() {
+	params := types.Params{
+		CreateMarketFee:     "1000",
+		MarketMakerFee:      "0.001",
+		MarketTakerFee:      "0.002",
+		MakerFeeDestination: "community_pool",
+		TakerFeeDestination: "burn",
+	}
+
+	msg := &types.MsgUpdateParams{
+		Authority: "",
+		Params:    params,
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().Error(err)
+	suite.Require().Nil(response)
+	suite.Require().Contains(err.Error(), "invalid authority")
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_ZeroFees() {
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	params := types.Params{
+		CreateMarketFee:     "0",
+		MarketMakerFee:      "0",
+		MarketTakerFee:      "0",
+		MakerFeeDestination: "",
+		TakerFeeDestination: "",
+	}
+
+	msg := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    params,
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+
+	// Verify zero fees were set
+	retrievedParams := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal("0", retrievedParams.CreateMarketFee)
+	suite.Require().Equal("0", retrievedParams.MarketMakerFee)
+	suite.Require().Equal("0", retrievedParams.MarketTakerFee)
+	suite.Require().Equal("", retrievedParams.MakerFeeDestination)
+	suite.Require().Equal("", retrievedParams.TakerFeeDestination)
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_DifferentDestinations() {
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	params := types.Params{
+		CreateMarketFee:     "500",
+		MarketMakerFee:      "0.0005",
+		MarketTakerFee:      "0.001",
+		MakerFeeDestination: "validator_rewards",
+		TakerFeeDestination: "community_pool",
+	}
+
+	msg := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    params,
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+
+	// Verify different destinations were set
+	retrievedParams := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal("validator_rewards", retrievedParams.MakerFeeDestination)
+	suite.Require().Equal("community_pool", retrievedParams.TakerFeeDestination)
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_HighFees() {
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	params := types.Params{
+		CreateMarketFee:     "10000",
+		MarketMakerFee:      "0.01", // 1%
+		MarketTakerFee:      "0.02", // 2%
+		MakerFeeDestination: "burn",
+		TakerFeeDestination: "burn",
+	}
+
+	msg := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    params,
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+
+	// Verify high fees were set
+	retrievedParams := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal("10000", retrievedParams.CreateMarketFee)
+	suite.Require().Equal("0.01", retrievedParams.MarketMakerFee)
+	suite.Require().Equal("0.02", retrievedParams.MarketTakerFee)
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_MultipleUpdates() {
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+
+	// First update
+	params1 := types.Params{
+		CreateMarketFee:     "100",
+		MarketMakerFee:      "0.0001",
+		MarketTakerFee:      "0.0002",
+		MakerFeeDestination: "community_pool",
+		TakerFeeDestination: "burn",
+	}
+
+	msg1 := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    params1,
+	}
+
+	response1, err := suite.msgServer.UpdateParams(suite.ctx, msg1)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response1)
+
+	// Verify first update
+	retrievedParams1 := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal(params1.CreateMarketFee, retrievedParams1.CreateMarketFee)
+
+	// Second update
+	params2 := types.Params{
+		CreateMarketFee:     "300",
+		MarketMakerFee:      "0.0003",
+		MarketTakerFee:      "0.0006",
+		MakerFeeDestination: "validator_rewards",
+		TakerFeeDestination: "community_pool",
+	}
+
+	msg2 := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    params2,
+	}
+
+	response2, err := suite.msgServer.UpdateParams(suite.ctx, msg2)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response2)
+
+	// Verify second update
+	retrievedParams2 := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal(params2.CreateMarketFee, retrievedParams2.CreateMarketFee)
+	suite.Require().Equal(params2.MarketMakerFee, retrievedParams2.MarketMakerFee)
+	suite.Require().Equal(params2.MarketTakerFee, retrievedParams2.MarketTakerFee)
+	suite.Require().Equal(params2.MakerFeeDestination, retrievedParams2.MakerFeeDestination)
+	suite.Require().Equal(params2.TakerFeeDestination, retrievedParams2.TakerFeeDestination)
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_PartialUpdate() {
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+
+	// Set initial params
+	initialParams := types.Params{
+		CreateMarketFee:     "100",
+		MarketMakerFee:      "0.001",
+		MarketTakerFee:      "0.002",
+		MakerFeeDestination: "community_pool",
+		TakerFeeDestination: "burn",
+	}
+
+	err := suite.k.SetParams(suite.ctx, initialParams)
+	suite.Require().NoError(err)
+
+	// Update with new params (all fields must be provided)
+	updatedParams := types.Params{
+		CreateMarketFee:     "150",               // Changed
+		MarketMakerFee:      "0.001",             // Keep same
+		MarketTakerFee:      "0.002",             // Keep same
+		MakerFeeDestination: "validator_rewards", // Changed
+		TakerFeeDestination: "burn",              // Keep same
+	}
+
+	msg := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    updatedParams,
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+
+	// Verify update
+	retrievedParams := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal("150", retrievedParams.CreateMarketFee)
+	suite.Require().Equal("0.001", retrievedParams.MarketMakerFee)
+	suite.Require().Equal("0.002", retrievedParams.MarketTakerFee)
+	suite.Require().Equal("validator_rewards", retrievedParams.MakerFeeDestination)
+	suite.Require().Equal("burn", retrievedParams.TakerFeeDestination)
+}
+
+func (suite *IntegrationTestSuite) TestMsgUpdateParams_EmptyParams() {
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+
+	msg := &types.MsgUpdateParams{
+		Authority: authority,
+		Params:    types.Params{}, // Empty params
+	}
+
+	response, err := suite.msgServer.UpdateParams(suite.ctx, msg)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+
+	// Verify empty params were set
+	retrievedParams := suite.k.GetParams(suite.ctx)
+	suite.Require().Equal("", retrievedParams.CreateMarketFee)
+	suite.Require().Equal("", retrievedParams.MarketMakerFee)
+	suite.Require().Equal("", retrievedParams.MarketTakerFee)
+	suite.Require().Equal("", retrievedParams.MakerFeeDestination)
+	suite.Require().Equal("", retrievedParams.TakerFeeDestination)
+}
--- x/tradebin/keeper/query_amm_test.go
+++ x/tradebin/keeper/query_amm_test.go
@@ -1,1 +1,350 @@
-package keeper
+package keeper_test
+
+import (
+	"cosmossdk.io/math"
+	"github.com/bze-alphateam/bze/x/tradebin/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (suite *IntegrationTestSuite) TestQueryAmm_LiquidityPool() {
+	liquidityPool := types.LiquidityPool{
+		Id:      "atom/usdc",
+		Base:    "atom",
+		Quote:   "usdc",
+		LpDenom: "lp/atom/usdc",
+		Creator: "bze1creator",
+		Fee:     math.LegacyMustNewDecFromStr("0.003"), // 0.3%
+		FeeDest: &types.FeeDestination{
+			Treasury:  math.LegacyMustNewDecFromStr("0.5"),
+			Burner:    math.LegacyMustNewDecFromStr("0.3"),
+			Providers: math.LegacyMustNewDecFromStr("0.2"),
+		},
+		ReserveBase:  math.NewInt(1000000),
+		ReserveQuote: math.NewInt(2000000),
+		Stable:       false,
+	}
+
+	suite.k.SetLiquidityPool(suite.ctx, liquidityPool)
+
+	req := &types.QueryLiquidityPoolRequest{
+		PoolId: "atom/usdc",
+	}
+
+	response, err := suite.k.LiquidityPool(suite.ctx, req)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+	suite.Require().NotNil(response.Pool)
+	suite.Require().Equal(liquidityPool.Id, response.Pool.Id)
+	suite.Require().Equal(liquidityPool.Base, response.Pool.Base)
+	suite.Require().Equal(liquidityPool.Quote, response.Pool.Quote)
+	suite.Require().Equal(liquidityPool.LpDenom, response.Pool.LpDenom)
+	suite.Require().Equal(liquidityPool.Creator, response.Pool.Creator)
+	suite.Require().Equal(liquidityPool.Fee, response.Pool.Fee)
+	suite.Require().Equal(liquidityPool.ReserveBase, response.Pool.ReserveBase)
+	suite.Require().Equal(liquidityPool.ReserveQuote, response.Pool.ReserveQuote)
+	suite.Require().Equal(liquidityPool.Stable, response.Pool.Stable)
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_LiquidityPoolNilRequest() {
+	response, err := suite.k.LiquidityPool(suite.ctx, nil)
+
+	suite.Require().Error(err)
+	suite.Require().Nil(response)
+	suite.Require().Equal(codes.InvalidArgument, status.Code(err))
+	suite.Require().Contains(err.Error(), "invalid request")
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_LiquidityPoolNotFound() {
+	req := &types.QueryLiquidityPoolRequest{
+		PoolId: "non-existent-pool",
+	}
+
+	response, err := suite.k.LiquidityPool(suite.ctx, req)
+
+	suite.Require().Error(err)
+	suite.Require().Nil(response)
+	suite.Require().Equal(codes.NotFound, status.Code(err))
+	suite.Require().Contains(err.Error(), "not found")
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_LiquidityPoolStable() {
+	liquidityPool := types.LiquidityPool{
+		Id:      "usdt/usdc",
+		Base:    "usdt",
+		Quote:   "usdc",
+		LpDenom: "lp/usdt/usdc",
+		Creator: "bze1creator",
+		Fee:     math.LegacyMustNewDecFromStr("0.001"), // 0.1%
+		FeeDest: &types.FeeDestination{
+			Treasury:  math.LegacyMustNewDecFromStr("0.6"),
+			Burner:    math.LegacyMustNewDecFromStr("0.4"),
+			Providers: math.LegacyMustNewDecFromStr("0.0"),
+		},
+		ReserveBase:  math.NewInt(5000000),
+		ReserveQuote: math.NewInt(4950000),
+		Stable:       true, // Stable pool
+	}
+
+	suite.k.SetLiquidityPool(suite.ctx, liquidityPool)
+
+	req := &types.QueryLiquidityPoolRequest{
+		PoolId: "usdt/usdc",
+	}
+
+	response, err := suite.k.LiquidityPool(suite.ctx, req)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+	suite.Require().NotNil(response.Pool)
+	suite.Require().Equal(liquidityPool.Id, response.Pool.Id)
+	suite.Require().True(response.Pool.Stable)
+	suite.Require().Equal(liquidityPool.FeeDest.Treasury, response.Pool.FeeDest.Treasury)
+	suite.Require().Equal(liquidityPool.FeeDest.Burner, response.Pool.FeeDest.Burner)
+	suite.Require().Equal(liquidityPool.FeeDest.Providers, response.Pool.FeeDest.Providers)
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_AllLiquidityPools() {
+	liquidityPools := []types.LiquidityPool{
+		{
+			Id:      "atom/usdc",
+			Base:    "atom",
+			Quote:   "usdc",
+			LpDenom: "lp/atom/usdc",
+			Creator: "bze1creator1",
+			Fee:     math.LegacyMustNewDecFromStr("0.003"),
+			FeeDest: &types.FeeDestination{
+				Treasury:  math.LegacyMustNewDecFromStr("0.5"),
+				Burner:    math.LegacyMustNewDecFromStr("0.5"),
+				Providers: math.LegacyMustNewDecFromStr("0.0"),
+			},
+			ReserveBase:  math.NewInt(1000000),
+			ReserveQuote: math.NewInt(2000000),
+			Stable:       false,
+		},
+		{
+			Id:      "eth/usdc",
+			Base:    "eth",
+			Quote:   "usdc",
+			LpDenom: "lp/eth/usdc",
+			Creator: "bze1creator2",
+			Fee:     math.LegacyMustNewDecFromStr("0.005"),
+			FeeDest: &types.FeeDestination{
+				Treasury:  math.LegacyMustNewDecFromStr("0.3"),
+				Burner:    math.LegacyMustNewDecFromStr("0.3"),
+				Providers: math.LegacyMustNewDecFromStr("0.4"),
+			},
+			ReserveBase:  math.NewInt(3000000),
+			ReserveQuote: math.NewInt(4000000),
+			Stable:       false,
+		},
+		{
+			Id:      "usdt/usdc",
+			Base:    "usdt",
+			Quote:   "usdc",
+			LpDenom: "lp/usdt/usdc",
+			Creator: "bze1creator3",
+			Fee:     math.LegacyMustNewDecFromStr("0.001"),
+			FeeDest: &types.FeeDestination{
+				Treasury:  math.LegacyMustNewDecFromStr("1.0"),
+				Burner:    math.LegacyMustNewDecFromStr("0.0"),
+				Providers: math.LegacyMustNewDecFromStr("0.0"),
+			},
+			ReserveBase:  math.NewInt(5000000),
+			ReserveQuote: math.NewInt(6000000),
+			Stable:       true,
+		},
+	}
+
+	for _, pool := range liquidityPools {
+		suite.k.SetLiquidityPool(suite.ctx, pool)
+	}
+
+	req := &types.QueryAllLiquidityPoolsRequest{
+		Pagination: nil,
+	}
+
+	response, err := suite.k.AllLiquidityPools(suite.ctx, req)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+	suite.Require().Len(response.List, 3)
+
+	// Verify all pools are present
+	poolIds := make(map[string]bool)
+	for _, pool := range response.List {
+		poolIds[pool.Id] = true
+	}
+
+	suite.Require().True(poolIds["atom/usdc"])
+	suite.Require().True(poolIds["eth/usdc"])
+	suite.Require().True(poolIds["usdt/usdc"])
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_AllLiquidityPoolsNilRequest() {
+	response, err := suite.k.AllLiquidityPools(suite.ctx, nil)
+
+	suite.Require().Error(err)
+	suite.Require().Nil(response)
+	suite.Require().Equal(codes.InvalidArgument, status.Code(err))
+	suite.Require().Contains(err.Error(), "invalid request")
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_AllLiquidityPoolsEmpty() {
+	req := &types.QueryAllLiquidityPoolsRequest{
+		Pagination: nil,
+	}
+
+	response, err := suite.k.AllLiquidityPools(suite.ctx, req)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+	suite.Require().Empty(response.List)
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_AllLiquidityPoolsPagination() {
+	liquidityPools := []types.LiquidityPool{
+		{
+			Id:      "atom/usdc",
+			Base:    "atom",
+			Quote:   "usdc",
+			LpDenom: "lp/atom/usdc",
+			Creator: "bze1creator1",
+			Fee:     math.LegacyMustNewDecFromStr("0.003"),
+			FeeDest: &types.FeeDestination{
+				Treasury:  math.LegacyMustNewDecFromStr("0.5"),
+				Burner:    math.LegacyMustNewDecFromStr("0.5"),
+				Providers: math.LegacyMustNewDecFromStr("0.0"),
+			},
+			ReserveBase:  math.NewInt(1000000),
+			ReserveQuote: math.NewInt(2000000),
+			Stable:       false,
+		},
+		{
+			Id:      "eth/usdc",
+			Base:    "eth",
+			Quote:   "usdc",
+			LpDenom: "lp/eth/usdc",
+			Creator: "bze1creator2",
+			Fee:     math.LegacyMustNewDecFromStr("0.005"),
+			FeeDest: &types.FeeDestination{
+				Treasury:  math.LegacyMustNewDecFromStr("0.3"),
+				Burner:    math.LegacyMustNewDecFromStr("0.3"),
+				Providers: math.LegacyMustNewDecFromStr("0.4"),
+			},
+			ReserveBase:  math.NewInt(3000000),
+			ReserveQuote: math.NewInt(4000000),
+			Stable:       false,
+		},
+	}
+
+	for _, pool := range liquidityPools {
+		suite.k.SetLiquidityPool(suite.ctx, pool)
+	}
+
+	req := &types.QueryAllLiquidityPoolsRequest{
+		Pagination: &query.PageRequest{
+			Limit: 1,
+		},
+	}
+
+	response, err := suite.k.AllLiquidityPools(suite.ctx, req)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+	suite.Require().Len(response.List, 1)
+	suite.Require().NotNil(response.Pagination)
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_AllLiquidityPoolsVariousFees() {
+	liquidityPools := []types.LiquidityPool{
+		{
+			Id:      "btc/usdc",
+			Base:    "btc",
+			Quote:   "usdc",
+			LpDenom: "lp/btc/usdc",
+			Creator: "bze1creator1",
+			Fee:     math.LegacyMustNewDecFromStr("0.01"), // 1%
+			FeeDest: &types.FeeDestination{
+				Treasury:  math.LegacyMustNewDecFromStr("0.2"),
+				Burner:    math.LegacyMustNewDecFromStr("0.3"),
+				Providers: math.LegacyMustNewDecFromStr("0.5"),
+			},
+			ReserveBase:  math.NewInt(1000000),
+			ReserveQuote: math.NewInt(2000000),
+			Stable:       false,
+		},
+		{
+			Id:      "dai/usdc",
+			Base:    "dai",
+			Quote:   "usdc",
+			LpDenom: "lp/dai/usdc",
+			Creator: "bze1creator2",
+			Fee:     math.LegacyMustNewDecFromStr("0.0001"), // 0.01%
+			FeeDest: &types.FeeDestination{
+				Treasury:  math.LegacyMustNewDecFromStr("0.0"),
+				Burner:    math.LegacyMustNewDecFromStr("0.0"),
+				Providers: math.LegacyMustNewDecFromStr("1.0"),
+			},
+			ReserveBase:  math.NewInt(3000000),
+			ReserveQuote: math.NewInt(4000000),
+			Stable:       true,
+		},
+	}
+
+	for _, pool := range liquidityPools {
+		suite.k.SetLiquidityPool(suite.ctx, pool)
+	}
+
+	req := &types.QueryAllLiquidityPoolsRequest{
+		Pagination: nil,
+	}
+
+	response, err := suite.k.AllLiquidityPools(suite.ctx, req)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+	suite.Require().Len(response.List, 2)
+
+	// Verify fee structures are included in response
+	for _, pool := range response.List {
+		if pool.Id == "btc/usdc" {
+			suite.Require().Equal(math.LegacyMustNewDecFromStr("0.01"), pool.Fee)
+			suite.Require().Equal(math.LegacyMustNewDecFromStr("0.2"), pool.FeeDest.Treasury)
+			suite.Require().Equal(math.LegacyMustNewDecFromStr("0.3"), pool.FeeDest.Burner)
+			suite.Require().Equal(math.LegacyMustNewDecFromStr("0.5"), pool.FeeDest.Providers)
+		} else if pool.Id == "dai/usdc" {
+			suite.Require().Equal(math.LegacyMustNewDecFromStr("0.0001"), pool.Fee)
+			suite.Require().Equal(math.LegacyMustNewDecFromStr("1.0"), pool.FeeDest.Providers)
+			suite.Require().True(pool.Stable)
+		}
+	}
+}
+
+func (suite *IntegrationTestSuite) TestQueryAmm_LiquidityPoolZeroReserves() {
+	liquidityPool := types.LiquidityPool{
+		Id:      "empty/pool",
+		Base:    "empty",
+		Quote:   "pool",
+		LpDenom: "lp/empty/pool",
+		Creator: "bze1creator",
+		Fee:     math.LegacyMustNewDecFromStr("0.003"),
+		FeeDest: &types.FeeDestination{
+			Treasury:  math.LegacyMustNewDecFromStr("1.0"),
+			Burner:    math.LegacyMustNewDecFromStr("0.0"),
+			Providers: math.LegacyMustNewDecFromStr("0.0"),
+		},
+		ReserveBase:  math.ZeroInt(),
+		ReserveQuote: math.ZeroInt(),
+		Stable:       false,
+	}
+
+	suite.k.SetLiquidityPool(suite.ctx, liquidityPool)
+
+	req := &types.QueryLiquidityPoolRequest{
+		PoolId: "empty/pool",
+	}
+
+	response, err := suite.k.LiquidityPool(suite.ctx, req)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(response)
+	suite.Require().NotNil(response.Pool)
+	suite.Require().Equal(math.ZeroInt(), response.Pool.ReserveBase)
+	suite.Require().Equal(math.ZeroInt(), response.Pool.ReserveQuote)
+}
