6db33ba06b82f4e36c2dfef4334c0e352d6f38f4
stefan.balea <stefan.balea@axsys.ro>
2025-05-30 01:27:51 +0300

    added migration for publishers

--- x/cointrunk/keeper/migrator.go
+++ x/cointrunk/keeper/migrator.go
@@ -4,6 +4,7 @@ import (
 	"cosmossdk.io/store/prefix"
 	"github.com/bze-alphateam/bze/x/cointrunk/exported"
 	v2 "github.com/bze-alphateam/bze/x/cointrunk/migrations/v2"
+	"github.com/bze-alphateam/bze/x/cointrunk/types"
 	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -20,15 +21,26 @@ func NewMigrator(k Keeper, ss exported.Subspace) Migrator {
 	}
 }
 
-// Migrate1to2 migrates the x/burner module state from the consensus version 1 to
-// version 2. Specifically, it takes the parameters that are currently stored
-// and managed by the x/params modules and stores them directly into the x/burner
-// module state.
 func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 	adapter := runtime.KVStoreAdapter(m.keeper.storeService.OpenKVStore(ctx))
 	store := prefix.NewStore(adapter, []byte{})
 
 	m.keeper.Logger().Info("migrating x/cointrunk state from consensus version 1 to version 2")
 
-	return v2.Migrate(ctx, store, m.legacySubspace, m.keeper.cdc)
+	err := v2.MigrateParams(ctx, store, m.legacySubspace, m.keeper.cdc)
+	if err != nil {
+		return err
+	}
+	m.keeper.Logger().Info("x/cointrunk params migrated from consensus version 1 to version 2")
+
+	pStore := m.keeper.getPrefixedStore(ctx, types.KeyPrefix(types.PublisherKeyPrefix))
+
+	err = v2.MigratePublishers(pStore, m.keeper.cdc)
+	if err != nil {
+		return err
+	}
+
+	m.keeper.Logger().Info("x/cointrunk publishers migrated from consensus version 1 to version 2")
+
+	return nil
 }
