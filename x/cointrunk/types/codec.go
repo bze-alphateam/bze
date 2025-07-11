73861c2cd84405ba48b3662d9fbab203901754de
stefan.balea <stefan.balea@axsys.ro>
2025-07-11 18:58:36 +0300

    try to fix v1 accepted domain proposal type

--- x/cointrunk/types/codec.go
+++ x/cointrunk/types/codec.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"github.com/bze-alphateam/bze/x/cointrunk/v1types"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
@@ -14,6 +15,7 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 		&MsgAcceptDomain{},
 		&MsgSavePublisher{},
 		&MsgUpdateParams{},
+		&v1types.AcceptedDomainProposal{},
 	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
