78a1009f30b7e276a91a90cbaa36a05df2eba807
stefan.balea <faneatiku@yahoo.com>
2025-12-07 12:27:14 +0200

    refactor: updated service fee handlers to simplify fee conversion logic and improve readability

--- x/txfeecollector/keeper/service_fees_handler.go
+++ x/txfeecollector/keeper/service_fees_handler.go
@@ -26,7 +26,7 @@ func (k Keeper) ConvertCommunityPoolFeesToNativeDenom(ctx sdk.Context) error {
 	}
 
 	moduleAddr := k.accountKeeper.GetModuleAddress(types.CpFeeCollector)
-	err = k.distrKeeper.FundCommunityPool(ctx, sdk.NewCoins(*toSend), moduleAddr)
+	err = k.distrKeeper.FundCommunityPool(ctx, *toSend, moduleAddr)
 
 	return err
 }
@@ -42,13 +42,13 @@ func (k Keeper) convertFeesAndSend(ctx sdk.Context, fromModule, toModule string)
 		return nil
 	}
 
-	err = k.bankKeeper.SendCoinsFromModuleToModule(ctx, fromModule, toModule, sdk.NewCoins(*toSend))
+	err = k.bankKeeper.SendCoinsFromModuleToModule(ctx, fromModule, toModule, *toSend)
 
 	return err
 }
 
 // convertFees converts all non-native module account fees into the native denomination and returns the total as a coin.
-func (k Keeper) convertFees(ctx sdk.Context, fromModule string) (*sdk.Coin, error) {
+func (k Keeper) convertFees(ctx sdk.Context, fromModule string) (*sdk.Coins, error) {
 	moduleAddr := k.accountKeeper.GetModuleAddress(fromModule)
 	allCoins := k.bankKeeper.GetAllBalances(ctx, moduleAddr)
 	if allCoins.IsZero() {
@@ -58,12 +58,10 @@ func (k Keeper) convertFees(ctx sdk.Context, fromModule string) (*sdk.Coin, erro
 
 	//group swappable coins to one collection
 	toSwap := sdk.NewCoins()
-	//put native coins directly to send collection.
-	//native coins should not reach this point, but just in case
-	var nativeCoin sdk.Coin
+	toSend := sdk.NewCoins()
 	for _, c := range allCoins {
 		if k.tradeKeeper.IsNativeDenom(ctx, c.Denom) {
-			nativeCoin = c
+			toSend = toSend.Add(c)
 			continue
 		}
 
@@ -76,24 +74,19 @@ func (k Keeper) convertFees(ctx sdk.Context, fromModule string) (*sdk.Coin, erro
 		}
 	}
 
-	if toSwap.IsZero() {
-		return nil, nil
-	}
+	if !toSwap.IsZero() {
+		swapped, err := k.tradeKeeper.ModuleSwapForNativeDenom(ctx, fromModule, toSwap)
+		if err != nil {
+			return nil, err
+		}
 
-	swapped, err := k.tradeKeeper.ModuleSwapForNativeDenom(ctx, fromModule, toSwap)
-	if err != nil {
-		return nil, err
-	}
-
-	if !nativeCoin.IsNil() && nativeCoin.IsPositive() {
-		swapped = swapped.Add(nativeCoin)
+		toSend = toSend.Add(swapped)
 	}
 
 	//if no swap happened we can try again next time
-	//this should not happen as the swap would return an error if the swap result is 0
-	if !swapped.IsPositive() {
+	if !toSend.IsAllPositive() {
 		return nil, nil
 	}
 
-	return &swapped, nil
+	return &toSend, nil
 }
