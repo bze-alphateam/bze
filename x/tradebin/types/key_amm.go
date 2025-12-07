de29af333e6c228415d75e4a483cf0af513b02b5
stefan.balea <faneatiku@yahoo.com>
2025-12-07 13:08:45 +0200

    refactor: removed unused LP modification queue and snapshot prefix logic

--- x/tradebin/types/key_amm.go
+++ x/tradebin/types/key_amm.go
@@ -2,27 +2,12 @@ package types
 
 const (
 	LpKeyPrefix = "lp/"
-
-	// LpSnapshotKeyPrefix is the prefix for storing LP snapshots
-	LpSnapshotKeyPrefix = "lps/"
-
-	// LpModificationQueuePrefix is the prefix for storing modified LP IDs
-	LpModificationQueuePrefix = "lpm/"
 )
 
 func LpPrefix() []byte {
 	return []byte(LpKeyPrefix)
 }
 
-func LpSnapshotPrefix() []byte {
-	return []byte(LpSnapshotKeyPrefix)
-}
-
 func PoolKey(poolId string) []byte {
-	return []byte(poolId)
-}
-
-// LpModificationQueueKey creates a key for a modified LP ID
-func LpModificationQueueKey(poolId string) []byte {
 	return []byte(poolId)
 }
